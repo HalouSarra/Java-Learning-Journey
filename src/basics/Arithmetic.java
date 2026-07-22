848054acd3ad9c524277749226cafac2f89f72ab
Sarra HALOU <halousarra@gmail.com>
2026-07-22 11:37:22 +0100

    basics>Arithmetic miniprojects>Mad Libs Game Separate concepts into packages and classes to be more redable

---
+++ src/basics/Arithmetic.java
@@ -0,0 +1,48 @@
+package basics;
+
+public class Arithmetic {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 3;
+        int z;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+        z = x % y;
+
+//        System.out.println(z);
+
+        //Augmented Assignment Operators
+//        x += y;//x = x + y;
+        x -= y;
+
+
+
+        System.out.println(x);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
