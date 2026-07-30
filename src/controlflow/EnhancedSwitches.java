850817ab55ae0b2cd08efd01e4c7d7c67b3d75f9
Sarra HALOU <halousarra@gmail.com>
2026-07-30 22:53:58 +0100

    miniprojects>TemperatureConverter

--- src/controlflow/EnhancedSwitches.java
+++ src/controlflow/EnhancedSwitches.java
@@ -1,4 +1,33 @@
 package controlflow;
 
+import java.util.Scanner;
+
 public class EnhancedSwitches {
+    public static void main(String[] args){
+        //Enhanced switch = A replacement to many else if statements
+        //                  (Java14 feature)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        String day = "Pizzaday";
+
+        switch (day){
+            case "Monday" -> System.out.println("It is a weekday!");
+            case "Tuesday" -> System.out.println("It is a weekday!");
+            case "Wednesday" -> System.out.println("It is a weekday!");
+            case "Thursday" -> System.out.println("It is a weekday!");
+            case "Friday" -> System.out.println("It is a weekday!");
+            case "Saturday" -> System.out.println("It is the weekend!");
+            case "Sunday" -> System.out.println("It is the weekend!");
+            default -> System.out.println(day + " is not a day!");
+
+
+
+
+
+        }
+
+
+
+
+    }
 }
