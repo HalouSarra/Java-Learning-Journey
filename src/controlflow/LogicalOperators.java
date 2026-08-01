3b0759a91563a39f9fb1524c260076d5d61fb5f6
Sarra HALOU <halousarra@gmail.com>
2026-08-01 07:05:18 +0100

    miniprojects>NumberGuessingGame controlflow>LogicalOperators>WhileLoops

--- src/controlflow/LogicalOperators.java
+++ src/controlflow/LogicalOperators.java
@@ -1,5 +1,42 @@
 package controlflow;
 
+import java.util.Scanner;
+
 public class LogicalOperators {
-    public static void
+    public static void main(String[] args){
+        // && = AND, || = OR, ! = NOT
+        double temp = -10;
+        boolean isSunny = true;
+
+        if(temp <= 30 && temp >= 0 && isSunny){
+            System.out.println("The weather is good");
+            System.out.println("It is SUNNY outside");
+        }
+        else if(temp <= 30 && temp >= 0 && !isSunny){
+            System.out.println("The weather is good");
+            System.out.println("It is CLOUDY outside");
+        }
+        else if(temp > 30 || temp < 0){
+            System.out.println("The weather is bad");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        //username must be between 4-12 characters
+        //username must not contain spaces or underscores
+        String username;
+        System.out.print("Enter you new username: ");
+        username = scanner.nextLine();
+        if(username.length() < 4 || username.length() > 12){
+            System.out.println("Username must be between 4-12 characters");
+        }
+        else if(username.contains(" ") || username.contains("_")){
+            System.out.println("Username must not contain spaces or underscores");
+        }
+        else {
+            System.out.println("Welcome " + username);
+        }
+
+        scanner.close();
+    }
 }
--- src/controlflow/WhileLoops.java
+++ src/controlflow/WhileLoops.java
@@ -1,4 +1,50 @@
 package controlflow;
 
+import java.util.Scanner;
+
 public class WhileLoops {
+    public static void main(String[] args){
+        //while loop = repeat some code forever
+        //             while some condition remains true
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isEmpty()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+
+        //Be care full from getting stuck in infinite loops!
+        String response = "";
+
+        while (!response.equals("Q")){
+            System.out.println("You are playing a game");
+            System.out.print("Press Q to quit: ");
+            response = scanner.next().toUpperCase();
+        }
+
+        System.out.println("You have quit the game");
+
+        int age = 0;
+
+        do{
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+            System.out.println("Your age can't be negative");
+        }while (age < 0);
+
+        System.out.println("You are " + age + " years old");
+
+        int number = 0;
+
+        do{
+            System.out.print("Enter a number between 1 - 10: ");
+            number = scanner.nextInt();
+        }while (number < 1 || number > 10);
+        System.out.println("You picked " + number);
+
+        scanner.close();
+    }
 }
