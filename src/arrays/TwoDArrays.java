37786c9b76724c570ce879f0654153ccf55937f4
Sarra HALOU <halousarra@gmail.com>
2026-08-05 12:11:10 +0100

    feat(miniprojects): add a simple console quiz game to practice arrays

--- src/arrays/TwoDArrays.java
+++ src/arrays/TwoDArrays.java
@@ -1,4 +1,34 @@
 package arrays;
 
 public class TwoDArrays {
+    public static void main(String[] args){
+//        String[] fruits = {"apples", "orange", "banana"};
+//        String[] vegetables = {"potato", "onion", "carrot"};
+//        String[] meats = {"chicken", "pork", "beef", "fish"};
+//        String[][] groceries = {fruits, vegetables, meats, {"candy", "cake", "cupcake", "lollipop"}};
+//
+//        groceries[0][0] = "pineapple";
+//
+//        for(String[] foods:groceries){
+//            for (String food : foods){
+//                System.out.print(food + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //mini project
+        char[][] telephone = {{'1', '2', '3'},
+                              {'4', '5', '6'},
+                              {'7', '8', '9'},
+                              {'*', '0', '#'}};
+
+        for(char[] row : telephone){
+            for (char c : row){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
--- src/controlflow/Varargs.java
+++ src/controlflow/Varargs.java
@@ -1,4 +1,28 @@
 package controlflow;
 
 public class Varargs {
+    public static void main(String[] args){
+        // variable arguments = allows a method to accept a varying type of arguments
+        //                      makes methods more flexible, no need for overloaded methods
+        //                      java will pack arguments into an array
+        //                      ... (ellipsis)
+
+        System.out.println(add(1,2,3,4,5,6));
+        System.out.println(average(1,2,3,4,5,6));
+
+    }
+    static double add(double... number){
+        double sum = 0;
+        for(double n:number){
+            sum +=n;
+        }
+        return sum;
+    }
+    static double average(double... number){
+        double sum = add(number);
+        if(number.length == 0){
+            return 0;
+        }
+        return sum / number.length;
+    }
 }
