489d143dd3eef968f8dde6bf2c4257e4d4259844
Sarra HALOU <halousarra@gmail.com>
2026-08-08 14:00:04 +0100

    feat(oop): add a simple examples about Array Of Objects and the CarCls class

--- src/oop/ArrayOfObjects.java
+++ src/oop/ArrayOfObjects.java
@@ -1,4 +1,24 @@
 package oop;
 
 public class ArrayOfObjects {
+    public static void main(String[] args){
+        CarCls car1 = new CarCls("Mustang", "Red");
+        CarCls car2 = new CarCls("Corvette", "Blue");
+        CarCls car3 = new CarCls("Charger", "Yellow");
+
+        CarCls[] cars = {car1, car2, car3,new CarCls("Picanto", "Black")};
+
+        for (CarCls car : cars) {
+            car.drive();
+        }
+        for (CarCls car : cars) {
+            car.color = "Black";
+        }
+        System.out.println();
+
+        for (CarCls car : cars) {
+            car.drive();
+        }
+
+    }
 }
