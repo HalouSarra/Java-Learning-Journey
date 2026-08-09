76038f6ad0b974e6517f13e6e2db0b1751047794
Sarra HALOU <halousarra@gmail.com>
2026-08-09 14:44:20 +0100

    feat(oop): add a simple examples about Overriding .tostring() method for objects

--- src/oop/CarCls.java
+++ src/oop/CarCls.java
@@ -1,15 +1,29 @@
 package oop;
 
 public class CarCls {
+    String make;
     String model;
+    int year;
     String color;
+
 
     CarCls(String model, String color){
         this.model = model;
         this.color = color;
     }
+    CarCls(String make, String model, int year, String color){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
 
     void drive(){
         System.out.println("You drive the " + this.color + " " + this.model);
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + year + " " + make + " " + model;
     }
 }
--- src/oop/ToStringMethod.java
+++ src/oop/ToStringMethod.java
@@ -1,4 +1,22 @@
 package oop;
 
 public class ToStringMethod {
+    public static void main(String[] args){
+        // .toString() = Method inherited from the Object class.
+        //               Used to return a string representation of an object.
+        //               By default, it returns a hash code as a unique identifier.
+        //               It can be overridden to provide meaningful detail.
+
+        CarCls car1 = new CarCls("Ford", "Mustang", 2025, "Red");
+        CarCls car2 = new CarCls("Chevrolet", "Corvette", 2026, "Blue");
+        Car car = new Car();
+
+        System.out.println(car);
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car1.toString());// you do not need to write the method name
+        System.out.println(car1.color + " " + car1.year + " " + car1.make + " " + car1.model);
+
+
+    }
 }
