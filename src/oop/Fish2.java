749ffbdb6622c9b8a1b181ecc16309f36f8bc304
Sarra HALOU <halousarra@gmail.com>
2026-08-10 12:33:06 +0100

    feat(oop): add a simple examples about Interfaces to identify the ked differences between interfaces, inheritance and abstract classes

---
+++ src/oop/Fish2.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Fish2 implements Prey, Predator {
+    @Override
+    public void flee(){
+        System.out.println("*The Fish is swimming away*");
+    }
+
+    @Override
+    public void hunt(){
+        System.out.println("*The Fish is hunting*");
+    }
+}
--- src/oop/Interfaces.java
+++ src/oop/Interfaces.java
@@ -1,4 +1,18 @@
 package oop;
 
 public class Interfaces {
+    public static void main(String[] args){
+        // Interface = A blueprint for a class that specifies a set of abstract methods
+        //             that implementation classes MUST define
+        //             Supports multiple inheritance-like behavior.
+        Rabbit rabbit = new Rabbit();
+        rabbit.flee();
+
+        Hawk hawk = new Hawk();
+        hawk.hunt();
+
+        Fish2 fish = new Fish2();
+        fish.flee();
+        fish.hunt();
+    }
 }
