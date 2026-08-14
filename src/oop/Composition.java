8e4b0bd05bc69052d6393efc5942979b26d11b58
Sarra HALOU <halousarra@gmail.com>
2026-08-14 12:22:26 +0100

    feat(oop): add a simple examples about Composition

--- src/oop/Composition.java
+++ src/oop/Composition.java
@@ -1,4 +1,15 @@
 package oop;
 
 public class Composition {
+    public static void main(String[] args){
+        // Composition = Represent a "part-of" relationship between objects.
+        //               For example, an Engine is "part of" a Car.
+        //               Allows complex objects to be constructed from smaller objects.
+
+        Car3 car = new Car3("Corvette", 2025, "V8");
+        System.out.println(car.model + " " + car.year + " " + car.engine.type);
+
+        car.start();
+
+    }
 }
