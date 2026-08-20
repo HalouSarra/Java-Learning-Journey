46d8759bd029b59264aba6eb1d8f65f4d24d8fd5
Sarra HALOU <halousarra@gmail.com>
2026-08-20 12:39:30 +0100

    feat(advanced): add a simple examples about reading files with BufferedReader and FileReader in the testRead.txt file

--- src/advanced/WriteFiles.java
+++ src/advanced/WriteFiles.java
@@ -13,7 +13,7 @@ public class WriteFiles {
         // PrintWriter = Best for structured data, like reports or logs
         // FileOutputStream = Best for binary files (e.g., images, audio files)
 
-        String filePath = "src/advanced/test.txt";
+        String filePath = "src/advanced/testWrite.txt";
         String textContent1 = "I like pizza!\nIt's really good!\nBuy me pizza!";
         String textContent2 = """
                 Roses are red
