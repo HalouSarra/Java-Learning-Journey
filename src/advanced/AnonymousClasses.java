aec03072d53083c0c2e5fe482e1eeef5a53b4281
Sarra HALOU <halousarra@gmail.com>
2026-08-22 22:01:50 +0100

    feat(advanced): Add a simple example about Anonymous Classes and when we should use them

--- src/advanced/AnonymousClasses.java
+++ src/advanced/AnonymousClasses.java
@@ -1,4 +1,25 @@
 package advanced;
 
 public class AnonymousClasses {
+    public static void main(String[] args){
+        // Anonymous class = A class that doesn't have a name. Cannot be reused.
+        //                   Add custom behavior without having to create a new class.
+        //                   Often used for one time uses (TimeTask, Runnable, callbacks)
+
+        // creat anonymous class instead of this
+        Dog dog1 = new Dog();
+        TalkingDog talkingDog = new TalkingDog();
+
+        //As following
+        Dog dog2 = new Dog(){//withing here we can define any unique features or override methods
+            @Override
+            void speak(){
+                System.out.println("Scooby Doo says *Ruh Ruh*");
+            }
+        };
+
+        dog1.speak();
+        talkingDog.speak();
+        dog2.speak();
+    }
 }
---
+++ src/advanced/Dog.java
@@ -0,0 +1,7 @@
+package advanced;
+
+public class Dog {
+    void speak(){
+        System.out.println("The dog goes *woof*");
+    }
+}
