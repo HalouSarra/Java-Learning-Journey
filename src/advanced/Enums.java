77c6e2d621d96c4b521b68b1c47070cf2dbdaa43
Sarra HALOU <halousarra@gmail.com>
2026-08-24 11:18:07 +0100

    feat(advanced): Add simple examples about enums

--- src/advanced/Enums.java
+++ src/advanced/Enums.java
@@ -1,4 +1,35 @@
 package advanced;
 
+import java.util.Scanner;
+
 public class Enums {
+    public static void main(String[] args){
+        // Enums = (Enumerations) A special kind of class that
+        //         represents a fixed set of constants.
+        //         They improve code readability and are easy to maintain.
+        //         More efficient with switches rather than comparing Strings.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
+        try {
+            Day day = Day.valueOf(response);
+//        System.out.println(day);
+//        System.out.println(day.getDayNumber());
+
+            switch (day){
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY,
+                     SUNDAY -> System.out.println("it is the weekend");
+            }
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Please enter a valid day");
+        }
+        scanner.close();
+    }
 }
