02bda138fd3a98facc10c3216fdbe619845efcd7
Sarra HALOU <halousarra@gmail.com>
2026-08-24 12:44:23 +0100

    feat(advanced): Add simple example about MultiThreading(introduction) and how to use it

--- src/advanced/Multithreading.java
+++ src/advanced/Multithreading.java
@@ -1,4 +1,27 @@
 package advanced;
 
 public class Multithreading {
+    public static void main(String[] args) {
+        // Multithreading = Enables a program to run multiple threads concurrently
+        //                  (Thread = A set of instructions that run independently)
+        //                  Useful for background tasks or time-consuming operations
+
+        Thread thread1 = new Thread(new MyRunnable2("PING"));
+        Thread thread2 = new Thread(new MyRunnable2("PONG"));
+
+        System.out.println("GAME START!");
+
+        thread1.start();
+        thread2.start();
+
+        //Make these threads wait for our main thread to finish
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("GAME OVER!");
+    }
 }
